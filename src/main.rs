6470f34d1ebd061d293961cb6a0065301207811b
PAUL SZABO <paul@tellme.social>
2025-01-02 17:37:21 -0800

    moar threads

--- src/main.rs
+++ src/main.rs
@@ -7,41 +7,35 @@ async fn main() {
     let client = redis::Client::open("redis://garnetlocal").unwrap();
     let mut con = client.get_multiplexed_async_connection().await.unwrap();
 
+    // Spawn a new task to periodically check the value of "foo"
+    tokio::spawn(async move {
+        let mut con2 = client.get_multiplexed_async_connection().await.unwrap();
+        loop {
+            check_foo(&mut con2).await;
+            sleep(Duration::from_secs(1)).await;
+        }
+    });
+
+
     // Increment the key "foo" and set an expiry of 5 seconds
     let _: () = con.incr("foo", 1).await.unwrap();
     let _: () = con.expire("foo", 5).await.unwrap();
 
-    // Create a timer that expires in 2 seconds
-    sleep(Duration::from_secs(2)).await;
-
-    // Read the value of "foo"
-    let result: Option<String> = con.get("foo").await.unwrap();
-
-    // Check if the value is null
-    match result {
-        Some(value) => println!("Value of 'foo': {}", value),
-        None => println!("Value of 'foo' is null"),
-    }
-
-    // Create a timer that expires in 5 seconds (total of 7)
-    sleep(Duration::from_secs(5)).await;
-
-    // Read the value of "foo"
-    let result: Option<String> = con.get("foo").await.unwrap();
-
-    // Check if the value is null
-    match result {
-        Some(value) => println!("Value of 'foo': {}", value),
-        None => println!("Value of 'foo' is null"),
-    }
+    sleep(Duration::from_secs(6)).await;
 
     // increment the key "foo" and check its value 
     let _: () = con.incr("foo", 1).await.unwrap();
-    sleep(Duration::from_secs(1)).await;
+    sleep(Duration::from_secs(4)).await;
+
+}
+
+async fn check_foo(con: &mut redis::aio::MultiplexedConnection) {
+    // Read the value of "foo"
     let result: Option<String> = con.get("foo").await.unwrap();
+
+    // Print the value of "foo"
     match result {
         Some(value) => println!("Value of 'foo': {}", value),
         None => println!("Value of 'foo' is null"),
     }
-
 }
